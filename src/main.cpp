d48f9afc8a5a1db68feb6a9d59f81d0fcf314dd9
VishalKumar9839 <110492128+VishalKumar9839@users.noreply.github.com>
2025-11-22 15:55:32 +0000

    main file changes \

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,42 @@
-#include <iostream>
-#include <string>
+// #include <iostream>
+// #include <string>
+// #include "server.h"
+
+// int main(int argc, char** argv) {
+//     std::string root = "./storage";
+//     int port = 8080;
+//     int threads = 8;
+
+//     if (argc > 1) port = std::stoi(argv[1]);
+//     if (argc > 2) root = argv[2];
+//     if (argc > 3) threads = std::stoi(argv[3]);
+
+//     Server server(port, root, threads);
+//     server.run();
+
+//     return 0;
+// }
 #include "server.h"
+#include <iostream>
 
 int main(int argc, char** argv) {
-    std::string root = "./storage";
+
     int port = 8080;
+    std::string root = "./storage";
     int threads = 8;
 
-    if (argc > 1) port = std::stoi(argv[1]);
-    if (argc > 2) root = argv[2];
-    if (argc > 3) threads = std::stoi(argv[3]);
+    if (argc >= 2) port = atoi(argv[1]);
+    if (argc >= 3) root = argv[2];
+    if (argc >= 4) threads = atoi(argv[3]);
 
-    Server server(port, root, threads);
+    std::string api_token = "";  // keep empty unless adding real authentication
+
+    std::cout << "Starting server on port " << port
+              << " root=" << root
+              << " threads=" << threads << "\n";
+
+    Server server(port, root, threads, api_token);
+
     server.run();
-
     return 0;
 }
